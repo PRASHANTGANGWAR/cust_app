246ffe88a6dd4202395f2ba3a987080ea8d5b163
Madhusudan Mishra <madhusudan.enbake@gmail.com>
2018-01-22 11:55:26 +0530

    Update localstorage after order creation and update

--- src/pages/current-order/current-order.ts
+++ src/pages/current-order/current-order.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { CategoriesPage } from '../categories/categories';
 import { EditOrderPage } from '../edit-order/edit-order';
-import { ConferenceData } from '../../providers/conference-data';
 import { Alerts } from '../../providers/alerts-provider';
 
 @Component({
@@ -17,23 +16,13 @@ export class CurrentOrderPage {
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    private confData:ConferenceData,
     private alert:Alerts) {
+
+    this.alert.showLoader();
     this.currentOrderData = this.navParams.get('currentOrder');
     this.currentOrderAddress = this.currentOrderData.address;
     this.orderPackages = this.currentOrderData.order_packages;
-
-    // for update items in localstorage @need to modify
-    this.alert.showLoader();
-    this.confData.getAllOrders().then((data:any)=>{
-    if(data.status == 200){
-        this.alert.hideLoader();
-    } else {
-      this.alert.hideLoader();
-      this.alert.presentToast(data.statusText);
-    }
-
-    })
+    this.alert.hideLoader();
   }
 
   ionViewDidLoad() {
